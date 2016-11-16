6ee9441c0c45d38b0ae0b4988ed1e86b7055ed2b
j4 <j464d2@gmail.com>
2016-11-15 20:13:31 -0500

    Changes before deploy

--- app/controllers/competitors_controller.rb
+++ app/controllers/competitors_controller.rb
@@ -1,4 +1,22 @@
 class CompetitorsController < ApplicationController
-  def index
+  def create
+    email = params[:competitor][:email]
+    score = params[:competitor][:score]. to_i
+    competitor = Competitor.find_by_email(email)
+
+    if competitor
+      competitor.update(competitor_params) if competitor.score < score
+    else
+      redirect_to competotirs_path
   end
+
+  def index
+    @competitors = Competitor.all.order(score: :desc).limit(10)
+end
+
+private
+
+def competitor_params
+  params.require(:competitor).permit(:email, :score)
+end
 end
